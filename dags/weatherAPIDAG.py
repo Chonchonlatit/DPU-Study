29a4fc4307b77bb8a4a9155bcac74d82f7ca0c4b
CLC <chon.chonlatit@gmail.com>
2025-04-06 21:00:47 +0000

    resudy

--- dags/weatherAPIDAG.py
+++ dags/weatherAPIDAG.py
@@ -1,7 +1,9 @@
 import json
+from datetime import timedelta
 
 from airflow import DAG
 from airflow.models import Variable
+from airflow.operators.email import EmailOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
@@ -10,10 +12,14 @@ from airflow.utils import timezone
 import requests
 
 
+DAG_FOLDER = "/opt/airflow/dags"
+
+
 def _get_weather_data():
-    API_KEY = "a721af6b5545eb25497f1edd07d1ee0c"
+    # assert 1 == 2
+
     # API_KEY = os.environ.get("WEATHER_API_KEY")
-    # API_KEY = Variable.get("weather_api_key")
+    API_KEY = Variable.get("weather_api_key")
 
     payload = {
         "q": "bangkok",
@@ -27,8 +33,14 @@ def _get_weather_data():
     data = response.json()
     print(data)
 
-    with open("/opt/airflow/dags/data.json", "w") as f:
+    with open(f"{DAG_FOLDER}/data.json", "w") as f:
         json.dump(data, f)
+
+def _validate_data():
+    with open(f"{DAG_FOLDER}/data.json", "r") as f:
+        data = json.load(f)
+
+    assert data.get("main") is not None
 
 def _create_weather_table():
     pg_hook = PostgresHook(
@@ -55,7 +67,7 @@ def _load_data_to_postgres():
     connection = pg_hook.get_conn()
     cursor = connection.cursor()
 
-    with open("/opt/airflow/dags/data.json", "r") as f:
+    with open(f"{DAG_FOLDER}/data.json", "r") as f:
         data = json.load(f)
 
     temp = data["main"]["temp"]
@@ -66,29 +78,50 @@ def _load_data_to_postgres():
     cursor.execute(sql)
     connection.commit()
 
+default_args = {
+    "email": ["kan@odds.team"],
+    "retries": 3,
+    "retry_delay": timedelta(minutes=1),
+}
+
 with DAG(
     "weather_api_dag",
-    schedule="@hourly",
+    default_args=default_args,
+    schedule="0 */3 * * *",
     start_date=timezone.datetime(2025, 2, 1),
-    tags=["dpu2"]
+    tags=["dpu2"],
 ):
     start = EmptyOperator(task_id="start")
-
+    
     get_weather_data = PythonOperator(
         task_id="get_weather_data",
         python_callable=_get_weather_data,
     )
-
+    
+    validate_data = PythonOperator(
+        task_id="validate_data",
+        python_callable=_validate_data,
+    )
+    
     create_weather_table = PythonOperator(
         task_id="create_weather_table",
         python_callable=_create_weather_table,
     )
-    
+
     load_data_to_postgres = PythonOperator(
         task_id="load_data_to_postgres",
         python_callable=_load_data_to_postgres,
     )
 
+    send_email = EmailOperator(
+        task_id="send_email",
+        to=["kan@odds.team"],
+        subject="Finished getting open weather data",
+        html_content="Done",
+    )
+
     end = EmptyOperator(task_id="end")
 
-    start >> get_weather_data >> create_weather_table >> load_data_to_postgres >> end
+    start >> get_weather_data >> validate_data >> load_data_to_postgres >> send_email
+    start >> create_weather_table >> load_data_to_postgres
+    send_email >> end
